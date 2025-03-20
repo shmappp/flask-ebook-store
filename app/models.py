0f0ef35eb49cf6b83851b1ee2c229229a51e934f
Shams <shamssaaticho9@gmail.com>
2025-03-20 23:19:47 +1100

    updated identifier field

--- app/models.py
+++ app/models.py
@@ -10,12 +10,12 @@ class Book(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     title: so.Mapped[str] = so.mapped_column(sa.String(255))
     author: so.Mapped[str] = so.mapped_column(sa.String(255))
-    isbn: so.Mapped[str] = so.mapped_column(sa.String(255), unique=True)
+    identifier: so.Mapped[str] = so.mapped_column(sa.String(255), unique=True)
     epub_file: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=True) # for debug purposes, keep nullable until upload required
     uploaded_at: so.Mapped[Optional[datetime]] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
 
     __table_args__ = (
-        UniqueConstraint('isbn', name='uq_isbn'), 
+        UniqueConstraint('identifier', name='uq_identifier'), 
     )
 
     def to_dict(self):
@@ -23,7 +23,7 @@ class Book(db.Model):
             "id": self.id,
             "title": self.title,
             "author": self.author,
-            "isbn": self.isbn,
+            "identifier": self.identifier,
             "epub_file": self.epub_file,
             "uploaded_at": self.uploaded_at.strftime('%Y-%m-%d %H:%M:%S')
         }
