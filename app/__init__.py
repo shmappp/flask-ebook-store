5f4f91363be616b7679e895e9dfc609289d65323
Shams <shamssaaticho9@gmail.com>
2025-03-21 00:10:30 +1100

    added CORS spec

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # permit cross origin resource sharing with all origins (#TODO: specify frontend later)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
