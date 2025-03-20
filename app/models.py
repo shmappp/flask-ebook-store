0b850cd6baa07304303a2870537b3bcec9599ecc
Shams <shamssaaticho9@gmail.com>
2025-03-20 16:12:01 +1100

    added isbn to Book model and remove/list book functionality

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class Book(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     title: so.Mapped[str] = so.mapped_column(sa.String(255))
     author: so.Mapped[str] = so.mapped_column(sa.String(255))
+    isbn: so.Mapped[str] = so.mapped_column(sa.String(255), unique=True)
     epub_file: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=True) # for debug purposes, keep nullable until upload required
     uploaded_at: so.Mapped[Optional[datetime]] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
 
@@ -17,6 +18,7 @@ class Book(db.Model):
             "id": self.id,
             "title": self.title,
             "author": self.author,
+            "isbn": self.isbn,
             "epub_file": self.epub_file,
             "uploaded_at": self.uploaded_at.strftime('%Y-%m-%d %H:%M:%S')
         }
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, jsonify
 from app import app, db
 from app.models import Book 
+import sqlalchemy as sa 
 
 @app.route('/')
 def index():
@@ -11,14 +12,30 @@ def upload():
     #file = request.file
     pass
 
-
 @app.route('/add_book', methods=['POST'])
 def add_book():
     data = request.get_json()
     book = Book(
         title=data.get('title'),
-        author=data.get('author')
+        author=data.get('author'),
+        isbn=data.get('isbn')
     )
     db.session.add(book)
     db.session.commit()
     return jsonify(book.to_dict()), 201
+
+@app.route('/remove_book', methods=['POST'])
+def remove_book():
+    # removes by isbn
+    data = request.get_json()
+    book =  db.session.scalar(sa.select(Book).where(Book.isbn == data.get('isbn')))
+    if book:
+        db.session.remove(book)
+        return jsonify(book.to_dict()), 200
+    else:
+        return jsonify({}), 204
+
+@app.route('/book_list', methods=['GET'])
+def book_list():
+    books = db.session.scalars(sa.select(Book)).all()
+    return jsonify([book.to_dict() for book in books]), 200
