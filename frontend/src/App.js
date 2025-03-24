456dbae464e0cc6693f8c9946fb207136b105fd7
Shams <shamssaaticho9@gmail.com>
2025-03-24 23:25:52 +1100

    added book list table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import DarkModeToggle from './components/DarkModeToggle.js';
+import BookTable from './components/BookTable.js'
 
 
 
@@ -8,10 +9,17 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>Dark mode test</h1>
-      <p><DarkModeToggle/></p>
-    </div>
+      <div className="App">
+      <h1>.epub store</h1>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+          <div style={{ margin: '0 auto', marginTop: '20px' }}>
+            <BookTable/>
+          </div>
+          <div style={{ position: 'absolute', right: '20px' }}>
+            <DarkModeToggle/>
+          </div>
+        </div>
+      </div>
   );
 }
 
---
+++ frontend/src/components/BookTable.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+
+const BookTable = () => {
+    const [bookData, setBookData] = useState([])
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/book_list`)
+            .then((response) => response.json())
+            .then((data) => setBookData(data))
+            .catch((error) => console.error("Error fetching data", error))
+    }, [])
+
+    return (
+        <table className='bookTable' border='1'>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>author</th>
+                    <th>identifier</th>
+                    <th>epub_file</th>
+                    <th>uploaded_at</th>
+                </tr>
+            </thead>
+            <tbody>
+                {bookData.length > 0 ? (
+                    bookData.map((book) => (
+                    <tr key={book.id}>
+                        <td>{book.id}</td>
+                        <td>{book.title}</td>
+                        <td>{book.author}</td>
+                        <td><a href={book.identifier}>{book.identifier}</a></td>
+                        <td>{book.epub_file}</td>
+                        <td>{book.uploaded_at}</td>
+                    </tr>
+                ))
+                ) : (
+                    <tr>
+                        <td colSpan='4'>No values found</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    );
+}
+
+export default BookTable;
