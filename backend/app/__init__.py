f0bcf118af97db5395786a9d6016871e46cae24c
Shams <shamssaaticho9@gmail.com>
2025-03-25 21:38:22 +1100

    added frontend to CORS allowed origins

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,6 +5,8 @@ from config import Config
 import os
 from flask_cors import CORS
 
+allowed_origins = ['http://localhost:3000'] # testing frontend origin
+
 app = Flask(__name__)
 CORS(app) # permit cross origin resource sharing with all origins (#TODO: specify frontend later)
 app.config.from_object(Config)
