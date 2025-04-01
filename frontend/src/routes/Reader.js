6234312ae668918b8e3a2d59192c0b75b9ed9167
Shams <shamssaaticho9@gmail.com>
2025-04-01 23:58:15 +1100

    added rendition callback

--- frontend/src/routes/Reader.js
+++ frontend/src/routes/Reader.js
@@ -6,13 +6,19 @@ import { ReactReader } from 'react-reader'
 export const Reader = () => {
     const [location, setLocation] = useState(0);
     const { id } = useParams();
+    //const rendition = useRef(null);
 
     return (
         <div style={{ height: '100vh' }}>
             <ReactReader
                 url={`${process.env.REACT_APP_BACKEND_URL}/book/${id}/file.epub`} // TODO: get rid of this hacky bullshit, why are we just appending file.epub and not using secondary arguments
                 location={location}
-                locationChanged={(epubcfi) => setLocation(epubcfi)}
+                //locationChanged={(epubcfi) => setLocation(epubcfi)}
+                getRendition={(rendition) => {
+                    rendition.on("locationChanged", (loc) => {
+                        setLocation(loc.start.cfi)
+                    })
+                }}
             />
         </div>
     )
