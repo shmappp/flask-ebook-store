0dee2933f2f57383d8c4e1a7c5a6365f46ff3db6
Shams <shamssaaticho9@gmail.com>
2025-04-02 22:56:07 +1100

    added return button

--- frontend/src/routes/Reader.js
+++ frontend/src/routes/Reader.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import '../App.css'
 import { useParams } from 'react-router'
 import { ReactReader } from 'react-reader'
+import { Link } from 'react-router'
 
 export const Reader = () => {
     const [location, setLocation] = useState(0);
@@ -9,19 +10,26 @@ export const Reader = () => {
     //const rendition = useRef(null);
 
     return (
-        <div style={{ height: '100vh' }}>
-            <ReactReader
-                url={`${process.env.REACT_APP_BACKEND_URL}/book/${id}/file.epub`} // TODO: get rid of this hacky bullshit, why are we just appending file.epub and not using secondary arguments
-                location={location}
-                locationChanged={(epubcfi) => setLocation(epubcfi)}
-                getRendition={(rendition) => { 
-                    rendition.on("locationChanged", (loc) => {
-                        console.log('New location: ', loc)
-                        setLocation(loc.start);
-                    })
-                }}
-            />
-        </div>
+        <>
+            <div className="flex h-screen">
+                <div className="w-1/3 p-4 border-r">
+                    <Link to="/" className="text-blue-500">back to library</Link>
+                </div>
+                <div style={{ height: '100vh' }}>
+                    <ReactReader
+                        url={`${process.env.REACT_APP_BACKEND_URL}/book/${id}/file.epub`} // TODO: get rid of this hacky bullshit, why are we just appending file.epub and not using secondary arguments
+                        location={location}
+                        locationChanged={(epubcfi) => setLocation(epubcfi)}
+                        getRendition={(rendition) => { 
+                            rendition.on("locationChanged", (loc) => {
+                                console.log('New location: ', loc)
+                                setLocation(loc.start);
+                            })
+                        }}
+                    />
+                </div>
+            </div>
+        </>
     )
 }
 
