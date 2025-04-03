c4e744d1ee37d22d6116c15488950b0886abacc7
Shams <shamssaaticho9@gmail.com>
2025-04-03 18:25:49 +1100

    added dark mode functionality to reader

--- frontend/src/routes/Reader.js
+++ frontend/src/routes/Reader.js
@@ -1,30 +1,58 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import '../App.css'
 import { useParams } from 'react-router'
-import { ReactReader } from 'react-reader'
+import { ReactReader, ReactReaderStyle, IReactReaderStyle } from 'react-reader'
 import { Link } from 'react-router'
+import DarkModeToggle from '../components/DarkModeToggle'
+import { getBootstrapTheme } from '../utils'
+
+
 
 export const Reader = () => {
     const [location, setLocation] = useState(0);
+    const [theme, setTheme] = useState(getBootstrapTheme());
+    const rendition = useRef(null);
     const { id } = useParams();
-    //const rendition = useRef(null);
+
+    useEffect(() => { 
+        const obs = new MutationObserver(() => {
+            const newTheme = getBootstrapTheme() 
+            setTheme(newTheme);
+            console.log('mutation set', newTheme)
+        });
+
+        obs.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: ["data-bs-theme"],
+          });
+        }, [])
+
+    useEffect(() => {
+        if (theme && rendition.current) {
+            updateTheme(rendition.current, theme);
+        }
+    }, [theme]);
+
 
     return (
         <>
-            <div className="flex h-screen">
-                <div className="w-1/3 p-4 border-r">
+            <div>
+                <div className="p-4 position-relative w-100 overflow-hidden">
                     <Link to="/" className="text-blue-500">back to library</Link>
+                    <span style = {{position: 'absolute', right: '20px'}}><DarkModeToggle/></span>
                 </div>
                 <div style={{ height: '100vh' }}>
                     <ReactReader
                         url={`${process.env.REACT_APP_BACKEND_URL}/book/${id}/file.epub`} // TODO: get rid of this hacky bullshit, why are we just appending file.epub and not using secondary arguments
                         location={location}
                         locationChanged={(epubcfi) => setLocation(epubcfi)}
-                        getRendition={(rendition) => { 
-                            rendition.on("locationChanged", (loc) => {
-                                console.log('New location: ', loc)
-                                setLocation(loc.start);
-                            })
+                        readerStyles={theme === 'dark' ? darkReaderTheme : lightReaderTheme}
+                        getRendition={(_rendition) => { 
+                            _rendition.on("locationChanged", (loc) => {
+                            setLocation(loc.start);
+                            });
+                            rendition.current = _rendition;
+                            updateTheme(_rendition, theme);
                         }}
                     />
                 </div>
@@ -32,5 +60,66 @@ export const Reader = () => {
         </>
     )
 }
+
+function updateTheme(rendition, theme) {
+    const themes = rendition.themes
+    switch (theme) {
+      case 'dark': {
+        themes.override('color', '#fff')
+        themes.override('background', '#000')
+        break
+      }
+      case 'light': {
+        themes.override('color', '#000')
+        themes.override('background', '#fff')
+        break
+      }
+    }
+  }
+
+const lightReaderTheme = {
+    ...ReactReaderStyle,
+    readerArea: {
+      ...ReactReaderStyle.readerArea,
+      transition: undefined,
+    },
+  }
+
+const darkReaderTheme = {
+    ...ReactReaderStyle,
+    arrow: {
+      ...ReactReaderStyle.arrow,
+      color: 'white',
+    },
+    arrowHover: {
+      ...ReactReaderStyle.arrowHover,
+      color: '#ccc',
+    },
+    readerArea: {
+      ...ReactReaderStyle.readerArea,
+      backgroundColor: '#000',
+      transition: undefined,
+    },
+    titleArea: {
+      ...ReactReaderStyle.titleArea,
+      color: '#ccc',
+    },
+    tocArea: {
+      ...ReactReaderStyle.tocArea,
+      background: '#111',
+    },
+    tocButtonExpanded: {
+      ...ReactReaderStyle.tocButtonExpanded,
+      background: '#222',
+    },
+    tocButtonBar: {
+      ...ReactReaderStyle.tocButtonBar,
+      background: '#fff',
+    },
+    tocButton: {
+      ...ReactReaderStyle.tocButton,
+      color: 'white',
+    },
+  }
 
 export default Reader;
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,3 +2,7 @@ export const checkUrl = (url) => {
     const pattern = /^(https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})/; 
     return pattern.test(url)
 }
+
+export const getBootstrapTheme = () => {
+    return document.documentElement.getAttribute("data-bs-theme");
+  };
