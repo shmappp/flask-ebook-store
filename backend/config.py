3f84a64e8e38eb5dc1a49cfa243f546fc833e3ca
Shams <shamssaaticho9@gmail.com>
2025-04-08 18:12:15 +1000

    added jwt specific config options

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
-
+from datetime import timedelta
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    JWT_SECRET_KEY= os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES= timedelta(minutes=int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES')))
