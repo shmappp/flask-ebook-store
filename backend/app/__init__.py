eef4369f562583f2cea45ee10c400a8ab72d946b
Shams <shamssaaticho9@gmail.com>
2025-04-08 18:12:48 +1000

    restricted CORS policy to frontend origin

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,11 +4,13 @@ from flask_migrate import Migrate
 from config import Config
 import os
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 allowed_origins = ['http://localhost:3000'] # testing frontend origin
 
 app = Flask(__name__)
-CORS(app) # permit cross origin resource sharing with all origins (#TODO: specify frontend later)
+CORS(app, origins=allowed_origins, supports_credentials=True)
+jwt = JWTManager(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
