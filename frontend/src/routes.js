ebece45bf1fe59a978aa1f709c85c6d2ef53e741
Shams <shamssaaticho9@gmail.com>
2025-04-08 18:14:13 +1000

    added login and register route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,13 @@
 import Home from "./routes/Home"
 import Reader from './routes/Reader'
-
+import Login from './routes/Login'
+import Register from "./routes/Register"
 
 export const routes = [
     { path: '/', element: <Home />},
-    { path: "read/:id", element: <Reader />}
+    { path: 'read/:id', element: <Reader />},
+    { path: '/login', element: <Login />},
+    { path: '/register', element: <Register />}
 ]
 
 export default routes;
